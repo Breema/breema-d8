8d687a6d857045cbd32723f12eb69a98d5a34fe2
Derek Wright <git@dwwright.net>
2018-01-19 16:22:08 -0800

    Footer: Custom block for Breema Center footer text.

---
+++ web/modules/custom/breema/src/Plugin/Block/BreemaFooterTextBlock.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\breema\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Url;
+use Drupal\node\Entity\NodeType;
+use Drupal\node\NodeTypeInterface;
+
+/**
+ * A site-wide block for Breema Center footer text.
+ *
+ * @Block(
+ *   id = "footer_text_block",
+ *   admin_label = @Translation("Breema Footer text"),
+ *   category = @Translation("Breema"),
+ * )
+ */
+class BreemaFooterTextBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return [
+      'copyright' => [
+        '#prefix' => '<div class="copyright">',
+        '#markup' => $this->t('Copyright © :year The Breema Center', [':year' => date('Y')]),
+        '#suffix' => '</div>',
+        '#weight' => 0,
+      ],
+      'address' => [
+        '#prefix' => '<div class="address"><address>',
+        '#markup' => $this->t('6076 Claremont Avenue, Oakland, CA 94618'),
+        '#suffix' => '</address></div>',
+        '#weight' => 1,
+      ],
+      'contact' => [
+        '#prefix' => '<div class="contact">',
+        '#markup' => '<a href="tel:+15104280937">510-428-0937</a> <a href="mailto:center@breema.com">center@breema.com</a>',
+        '#suffix' => '</div>',
+        '#weight' => 2,
+      ],
+      'service-mark' => [
+        '#prefix' => '<div class="service-mark">',
+        '#markup' => $this->t('Breema® is a service mark of The Breema Center'),
+        '#suffix' => '</div>',
+        '#weight' => 3,
+      ],
+    ];
+  }
+}
