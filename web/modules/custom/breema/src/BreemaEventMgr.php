c1ba0fe86df917ed7d205a9f44894adea3c19e68
Derek Wright <git@dwwright.net>
2018-11-09 09:51:42 -0500

    Event: Keep field_upcoming_events updated automatically on directory entries.

--- web/modules/custom/breema/src/BreemaEventMgr.php
+++ web/modules/custom/breema/src/BreemaEventMgr.php
@@ -14,15 +14,29 @@ use Drupal\node\NodeInterface;
 class BreemaEventMgr {
 
   /**
-   * Finds all place nodes used by upcoming or in-progress events.
+   * Nested array of place nodes used by upcoming events.
    *
-   * @return array
-   *   Nested array, indexed by node IDs of places that have active / upcoming
-   *   events, where the value is a sub-array of the node IDs of the next 3
-   *   events at that location, or an empty array if there are no such places.
+   * Top-level array is indexed by node IDs of places, values are a sub-array of
+   * the node IDs of the next 3 events (at most) at that location.
    */
-  public function getActivePlaces() {
-    $active_places = [];
+  protected $activePlaces;
+
+  /**
+   * Nested array of directory_entry nodes of instructors of upcoming events.
+   *
+   * Top-level array is indexed by node IDs of directory entries, values are a
+   * sub-array of the node IDs of the next 3 events (at most) taught by the
+   * instructor.
+   */
+  protected $activeInstructors;
+
+  /**
+   * Finds all place nodes and directory entries used by upcoming events.
+   */
+  public function __construct() {
+    $this->activePlaces = [];
+    $this->activeInstructors = [];
+
     $now = new DrupalDateTime('now');
     $now->setTimezone(new \DateTimeZone(DATETIME_STORAGE_TIMEZONE));
     $query = \Drupal::entityQuery('node');
@@ -40,66 +54,91 @@ class BreemaEventMgr {
       foreach ($events as $event) {
         $location = $event->get('field_location')->getValue();
         $location_id = $location[0]['target_id'];
-        if (empty($active_places[$location_id]) || count($active_places[$location_id]) < 3) {
-          $active_places[$location_id][] = $event->id();
+        if (empty($this->activePlaces[$location_id]) || count($this->activePlaces[$location_id]) < 3) {
+          $this->activePlaces[$location_id][] = $event->id();
+        }
+        $instructors = $event->get('field_instructors')->referencedEntities();
+        if (!empty($instructors)) {
+          foreach ($instructors as $instructor) {
+            $directory_entry = $instructor->get('field_directory_entry')->getValue();
+            if (!empty($directory_entry)) {
+              $nid = $directory_entry[0]['target_id'];
+              if (empty($this->activeInstructors[$nid]) || count($this->activeInstructors[$nid]) < 3) {
+                $this->activeInstructors[$nid][] = $event->id();
+              }
+            }
+          }
         }
       }
     }
-    return $active_places;
   }
 
+  /**
+   * Update all event-related nodes that aren't in sync with reality.
+   */
+  public function update() {
+    foreach (['place', 'directory_entry'] as $node_type) {
+      $this->updateData($node_type);
+    }
+  }
 
   /**
-   * Update field_has_active_event for any place node out of sync with a list.
+   * Update nodes of a given type that aren't in sync with reality.
    *
-   * @param array $active_places
-   *   Nested array of places with active events. Top-level index is node IDs
-   *   (nids) of place nodes, and the subarrays are the next 3 event node IDs at
-   *   that location.
+   * @param string $node_type
+   *   The node type (bundle) to update. Should be either 'place' or
+   *   'directory_entry'
    */
-  public function updateActivePlaces(array $active_places = NULL) {
-    if (!isset($active_places)) {
-      $active_places = $this->getActivePlaces();
+  public function updateData($node_type) {
+    $property_names = [
+      'place' => 'activePlaces',
+      'directory_entry' => 'activeInstructors',
+    ];
+    if (empty($property_names[$node_type])) {
+      /// @todo Throw an exception or something?
+      return;
     }
-    if (empty($active_places)) {
-      $stale_place_ids = \Drupal::entityQuery('node')
-        ->condition('type', 'place')
+    $property_name = $property_names[$node_type];
+
+    if (empty($this->$property_name)) {
+      $stale_ids = \Drupal::entityQuery('node')
+        ->condition('type', $node_type)
         ->condition('field_has_active_event', 1, '=')
         ->execute();
     }
     else {
-      $stale_place_ids = \Drupal::entityQuery('node')
-        ->condition('type', 'place')
+      $stale_ids = \Drupal::entityQuery('node')
+        ->condition('type', $node_type)
         ->condition('field_has_active_event', 1, '=')
-        ->condition('nid', array_keys($active_places), 'NOT IN')
+        ->condition('nid', array_keys($this->$property_name), 'NOT IN')
         ->execute();
     }
-    if (!empty($stale_place_ids)) {
-      $stale_places = Node::loadMultiple($stale_place_ids);
-      if (!empty($stale_places)) {
-        foreach ($stale_places as $stale_place) {
-          $stale_place->setNewRevision(FALSE);
-          $stale_place->set('field_has_active_event', 0);
-          $stale_place->set('field_upcoming_events', []);
-          $stale_place->save();
+    if (!empty($stale_ids)) {
+      $stale_nodes = Node::loadMultiple($stale_ids);
+      if (!empty($stale_nodes)) {
+        foreach ($stale_nodes as $stale_node) {
+          $stale_node->setNewRevision(FALSE);
+          $stale_node->set('field_has_active_event', 0);
+          $stale_node->set('field_upcoming_events', []);
+          $stale_node->save();
         }
       }
     }
-    if (!empty($active_places)) {
-      $active_place_nodes = Node::loadMultiple(array_keys($active_places));
-      if (!empty($active_place_nodes)) {
-        foreach ($active_place_nodes as $nid => $active_place) {
-          $has_active = $active_place->get('field_has_active_event')->value;
-          $upcoming = $active_place->get('field_upcoming_events')->getValue();
+    if (!empty($this->$property_name)) {
+      $active_nodes = Node::loadMultiple(array_keys($this->$property_name));
+      if (!empty($active_nodes)) {
+        foreach ($active_nodes as $nid => $active_node) {
+          $has_active = $active_node->get('field_has_active_event')->value;
+          $upcoming = $active_node->get('field_upcoming_events')->getValue();
           $upcoming_nids = [];
           foreach ($upcoming as $upcoming_event) {
             $upcoming_nids[] = $upcoming_event['target_id'];
           }
-          if (!$has_active || $upcoming_nids != $active_places[$nid]) {
-            $active_place->setNewRevision(FALSE);
-            $active_place->set('field_has_active_event', 1);
-            $active_place->set('field_upcoming_events', $active_places[$nid]);
-            $active_place->save();
+          if (!$has_active || $upcoming_nids != $this->$property_name[$nid]) {
+            $active_node->setNewRevision(FALSE);
+            $active_node->set('field_has_active_event', 1);
+            $active_node->set('field_upcoming_events', $this->$property_name[$nid]);
+            $active_node->save();
           }
         }
       }
