6324d596052ccc4b6eaf685120cd2313ab4b1c46
Derek Wright <git@dwwright.net>
2018-12-09 09:13:53 -0800

    User: Move directory_entry action links into shared helper:
    - Use the helper when viewing a user profile to inject action links
      near the directory entry being displayed.
    - 'Your Breema Directory entry' block uses the shared helper.
    - Helper is smart about checking access for the current user.

--- web/modules/custom/breema/src/Plugin/Block/BreemaDirectoryEntryBlock.php
+++ web/modules/custom/breema/src/Plugin/Block/BreemaDirectoryEntryBlock.php
@@ -4,8 +4,6 @@ namespace Drupal\breema\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Url;
-use Drupal\node\Entity\NodeType;
-use Drupal\node\NodeTypeInterface;
 use Drupal\user\Entity\User;
 
 
@@ -37,40 +35,17 @@ class BreemaDirectoryEntryBlock extends BlockBase {
     ];
     $current_user = \Drupal::currentUser();
     $current_user_entity = User::load($current_user->id());
-    $current_page = Url::fromRoute('<current>');
-    $url_options['query']['destination'] = $current_page->toString();
     $directory_entries = $current_user_entity->get('field_directory_entry')->referencedEntities();
     if (!empty($directory_entries)) {
       $directory_entry = array_pop($directory_entries);
+      $links = _breema_get_directory_entry_action_links($directory_entry);
       $view_builder = \Drupal::entityManager()->getViewBuilder('node');
-      $edit_url = Url::fromRoute('entity.node.edit_form', ['node' => $directory_entry->id()], $url_options);
-      $delete_url = Url::fromRoute('entity.node.delete_form', ['node' => $directory_entry->id()], $url_options);
       return $block + [
         'teaser' => $view_builder->view($directory_entry, 'teaser'),
-        'edit-link' => [
-          '#prefix' => '<div class="action-secondary">',
-          '#markup' => $this->t('<a href=":edit">Edit</a>', [':edit' => $edit_url->toString()]),
-          '#suffix' => '</div>',
-        ],
-        'delete-link' => [
-          // For reasons that aren't yet clear, #prefix and #suffix aren't
-          // working for this, so we directly stuff those into #markup.
-          '#markup' => '<div class="danger">' . $this->t('<a href=":delete" class="danger">Delete</a>', [':delete' => $delete_url->toString()]) . '</div>',
-        ],
-      ];
+      ] + $links;
     }
     else {
-      $node_type = NodeType::load('directory_entry');
-      $result = \Drupal::service('access_check.node.add')->access($current_user, $node_type);
-      if ($result) {
-        $directory_url = Url::fromUri('base:/directory');
-        $add_entry_url = Url::fromRoute('node.add', ['node_type' => 'directory_entry'], $url_options);
-        $markup = '<p>' . $this->t('You do not have an entry in the <a href=":breema-directory">International Breema directory</a>.', [':breema-directory' => $directory_url->toString()]) . '</p>';
-        $markup .= '<div class="action">' . $this->t('<a href=":create-directory-entry">Create entry</a>', [':create-directory-entry' => $add_entry_url->toString()]) . '</div>';
-        return $block + [
-          '#markup' => $markup,
-        ];
-      }
+      return $block + _breema_get_directory_entry_action_links();
     }
   }
 }
