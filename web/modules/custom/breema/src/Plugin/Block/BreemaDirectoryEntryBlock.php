0523a6edecb9a0ceaa6ce182cd6cba8b4c1b4a9e
Derek Wright <git@dwwright.net>
2018-12-17 09:27:14 -0800

    User: Pass entity to _breema_get_directory_entry_warning() so edit + delete can be links.

--- web/modules/custom/breema/src/Plugin/Block/BreemaDirectoryEntryBlock.php
+++ web/modules/custom/breema/src/Plugin/Block/BreemaDirectoryEntryBlock.php
@@ -35,20 +35,20 @@ class BreemaDirectoryEntryBlock extends BlockBase {
     ];
     $current_user = \Drupal::currentUser();
     $current_user_entity = User::load($current_user->id());
-    $directory_entries = $current_user_entity->get('field_directory_entry')->referencedEntities();
-    if (!empty($directory_entries)) {
-      $directory_entry = array_pop($directory_entries);
-      if (!$directory_entry->isPublished()) {
+    $entries = $current_user_entity->get('field_directory_entry')->referencedEntities();
+    if (!empty($entries)) {
+      $entry = array_pop($entries);
+      if (!$entry->isPublished()) {
         $directory_url = Url::fromUri('base:/directory');
         $block['warning'] = [
-          '#markup' => _breema_get_directory_entry_warning('block'),
+          '#markup' => _breema_get_directory_entry_warning('block', $entry),
           '#prefix' => '<div class="messages messages--warning">',
           '#suffix' => '</div>',
         ];
       }
       $view_builder = \Drupal::entityManager()->getViewBuilder('node');
-      $block['teaser'] = $view_builder->view($directory_entry, 'teaser');
-      $block += _breema_get_directory_entry_action_links($directory_entry);
+      $block['teaser'] = $view_builder->view($entry, 'teaser');
+      $block += _breema_get_directory_entry_action_links($entry);
     }
     else {
       $block += _breema_get_directory_entry_action_links(NULL, TRUE);
