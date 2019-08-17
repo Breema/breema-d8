2b079abba8c9b43d95b649830488e7bfa9582b44
Derek Wright <git@dwwright.net>
2019-08-17 12:44:39 -0700

    Migrate: Remove shortcut-related migrations (complete).

--- web/modules/custom/breema_migrate/bin/3-shortcuts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Sets are already defined in config, don't need migration.
-
-# Define links for each set.
-drush mim breema_csv_shortcut
-
-# Define the right set for each user based on certification level.
-drush mim breema_csv_shortcut_user
