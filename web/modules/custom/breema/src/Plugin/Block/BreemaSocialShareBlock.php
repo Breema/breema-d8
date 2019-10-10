Breema/breema-d8

commit 3765322cad2b74278bdef79a0c46d87eb95e1875

Share: Don't automatically open share links in new tabs (let the user decide).

web/modules/custom/breema/src/Plugin/Block/BreemaSocialShareBlock.php
@@ -53,7 +53,7 @@ class BreemaSocialShareBlock extends BlockBase {
       }
       foreach ($services as $service_id => $service_data) {
         $block[$group]['#items'][] = [
-          '#prefix' => '<a href="' . $service_data['link'] . '" title="' . $service_data['text'] . '" class="' . $service_id . '" target="_blank">',
+          '#prefix' => '<a href="' . $service_data['link'] . '" title="' . $service_data['text'] . '" class="' . $service_id . '>',
           '#markup' => $this->getLinkMarkup($group, $service_id, $service_data['text']),
           '#suffix' => '</a>',
         ];

commit ab6224cf37343489432812e7a516949269bc0424

Share: Fix the (+) link again (broken in commit 3765322cad2).

web/modules/custom/breema/src/Plugin/Block/BreemaSocialShareBlock.php
@@ -53,7 +53,7 @@ class BreemaSocialShareBlock extends BlockBase {
       }
       foreach ($services as $service_id => $service_data) {
         $block[$group]['#items'][] = [
-          '#prefix' => '<a href="' . $service_data['link'] . '" title="' . $service_data['text'] . '" class="' . $service_id . '>',
+          '#prefix' => '<a href="' . $service_data['link'] . '" title="' . $service_data['text'] . '" class="' . $service_id . '">',
           '#markup' => $this->getLinkMarkup($group, $service_id, $service_data['text']),
           '#suffix' => '</a>',
         ];