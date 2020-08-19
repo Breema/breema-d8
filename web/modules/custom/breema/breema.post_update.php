1323c9f84dab9c8d6cae0075701e76428e07b124
Derek Wright <git@dwwright.net>
2020-08-19 08:43:25 -1000

    Audio: Split the post_update to convert 'Dear student' into separate function, and (for now) disable the update to convert the other listening groups.

--- web/modules/custom/breema/breema.post_update.php
+++ web/modules/custom/breema/breema.post_update.php
@@ -8,11 +8,24 @@
 use Drupal\group\Entity\Group;
 
 /**
- * Converts listening groups into type 'audio'.
+ * Converts 'Dear student' listening group into type 'audio'.
  */
-function breema_post_update_convert_audio_groups() {
+function breema_post_update_convert_dear_student_group() {
+  // For the purposes of this update, we can hard-code this.
+  $group = Group::load(5);
+  // This approach work because of patch [#2923755-7] Ability to change group
+  // type after group creation: update all GroupContent types automatically.
+  // @see https://www.drupal.org/project/group/issues/2923755
+  $group->type = 'audio';
+  $group->save();
+}
+
+/**
+ * Converts the remaining listening groups into type 'audio'.
+ */
+function __breema_post_update_convert_audio_groups() {
   // For the purposes of this update, we can hard-code the listening groups.
-  for ($i = 5; $i <= 7; $i++) {
+  for ($i = 6; $i <= 7; $i++) {
     $group = Group::load($i);
     // This approach work because of patch [#2923755-7] Ability to change group
     // type after group creation: update all GroupContent types automatically.
